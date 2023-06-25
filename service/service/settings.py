a77fc5599a265c15811a61a21f5e65e0cc0b0805
Eugene Kolesov <wenturalox@gmail.com>
2023-06-25 18:21:45 +0200

    Day 8 - 1

--- service/service/settings.py
+++ service/service/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'cachalot',
     'clients',
     'services',
 ]
@@ -150,3 +151,13 @@ if DEBUG:
     }
 
 CELERY_BROKER_URL = 'redis://redis:6379/0'
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379/1',
+    }
+}
+
+PRICE_CACHE_NAME = 'price_cache'
--- service/services/views.py
+++ service/services/views.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.db.models import Prefetch, F, Sum
 from django.shortcuts import render
+from django.core.cache import cache
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
@@ -28,11 +30,19 @@ class SubscriptionView(ReadOnlyModelViewSet):
         response = super().list(request, *args, **kwargs)
         # через super мы обратились к месту откуда переопределили метод, т.е к базовому классу
 
+        price_cache = cache.get(settings.PRICE_CACHE_NAME)
+
+        if price_cache:
+            total_price = price_cache
+        else:
+            total_price = queryset.aggregate(total=Sum('price')).get('total')
+            cache.set(settings.PRICE_CACHE_NAME, total_price, 60 * 60)
+
         """ Агрегационные функции в SQL позволяют вывести суммарную инфу по группе записей (прямое отличие от annotate). Например 
         получая группу Subscription, можем по всей группе посчитать суммарные значения (наприм. сумму всех заказов, минимальный заказ, тд.)"""
         response_data = {'result': response.data}
 
-        response_data['total_amout'] = queryset.aggregate(total=Sum('price')).get('total')
+        response_data['total_amout'] = total_price
         """ Чтобы подсчитать сумму цен на питоне, пришлось бы проитерировать все выводы response и сложить значения в
         переменную, а здесь мы это делаем при помощи функции базы (Sum), но чтобы это было возможно, мы должны 
         с аннотировать и потом с агрегировать уже с анотированные данные """
